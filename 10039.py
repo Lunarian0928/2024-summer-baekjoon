48d1ba1245f5b7ef271342ff7eaa49e234064fca
Lunarian0928 <thanks092834@gmail.com>
2024-07-30 12:15:38 +0900

    10039번: 평균 점수

---
+++ 10039.py
@@ -0,0 +1,13 @@
+length = 5 # 학생 수
+# 동적으로 리스트 생성
+scores = list(range(0, length))
+
+sum = 0
+for i in range(0, length):
+    scores[i] = int(input())
+    # 문제 조건에 맞춰 특정 학생은 40점으로 설정
+    if (scores[i] < 40):
+        scores[i] = 40 
+    sum += scores[i]
+
+print(sum // length) # 평균
