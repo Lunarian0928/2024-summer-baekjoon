df20824ab480b319e6517d7315daa9ceaba68d87
Lunarian0928 <thanks092834@gmail.com>
2024-07-30 15:46:25 +0900

    2480번: 주사위 세개

--- 2480.py
+++ 2480.py
@@ -1,5 +1,6 @@
 d1, d2, d3 = map(int, input().split())
 
+# 최대를 구하는 함수
 def greatest(d1, d2, d3):
     if (d1 > d2) and (d1 > d3):
         return d1
@@ -8,15 +9,15 @@ def greatest(d1, d2, d3):
     else:
         return d3
     
-if (d1 == d2 == d3):
+if (d1 == d2 == d3): # 같은 눈 3개가 나온 경우
     print(10000 + d1 * 1000)
-elif (d1 != d2 != d3):
-    print(d1 * 100)
-else:
+# 모두 다른 눈이 나오는 경우
+elif (d1 != d2) and (d1 != d3) and (d2 != d3):
+    print(greatest(d1, d2, d3) * 100)
+else: # 같은 눈이 2개만 나오는 경우
     if (d1 == d2):
         print(1000 + d1 * 100)
     elif (d1 == d3):
         print(1000 + d1 * 100)
     else:
         print(1000 + d2 * 100)
-    
