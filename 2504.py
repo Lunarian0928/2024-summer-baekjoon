33c5b86d01398bc3c96d7bab799781a12b9ea6da
Lunarian0928 <thanks092834@gmail.com>
2024-08-02 20:08:05 +0900

    10799번: 쇠막대기

---
+++ 2504.py
@@ -0,0 +1,22 @@
+# 스택 생성자
+def stack():
+    return []
+
+# 스택이 비어있는지 여부를 반환하는 함수
+def is_empty(st):
+    return (len(st) == 0)
+
+# 스택에 요소를 푸시(push)하는 함수
+def push(st, e):
+    st.append(e)
+
+# 스택의 탑(top)을 반환하는 함수
+def top(st):
+    return st[-1]
+
+# 스택의 탑(top)을 제거하고 반환하는 함수
+def pop(st):
+    return st.pop()
+    
+ps = input()
+st = stack()
