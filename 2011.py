68aa13f0a3e9bfe329a1464a312c24123889bdb2
Lunarian0928 <thanks092834@gmail.com>
2024-08-13 10:21:06 +0900

    2011번: 암호코드

--- 2011.py
+++ 2011.py
@@ -1,22 +1,31 @@
 code = input().strip()
 
-arr = list(map(int, code))
+# 암호가 '0'으로 시작할 경우 해석 불가
+if code[0] == '0':
+    print(0)
+    exit(0)
 
-dp = [1 for _ in range(len(code) + 1)]
+arr = list(map(int, code))
+n = len(arr)
+
+dp = [0] * (n + 1)
+dp[0] = 1
 dp[1] = 1
 
-divisor = 10 ** 6 # 제수
-for i in range(2, len(code) + 1):
+divisor = 10 ** 6
+
+for i in range(2, n + 1):
     single_digit = arr[i - 1]
     double_digit = arr[i - 2] * 10 + arr[i - 1]
-    
+
+    # 1자리 문자를 제외한 앞의 i-1 문자를 표현하는 방법
     if 1 <= single_digit <= 9:
-        # 1자리 문자를 제외한 앞의 i-1 문자를 표현하는 방법
-        dp[i] += dp[i - 1] 
-        dp[i] %= divisor
-    if 10 <= double_digit <= 26:
-        # 2자리 문자를 제외한 앞의 i-2 문자를 표현하는 방법
-        dp[i] += dp[i - 2]
+        dp[i] += dp[i - 1]
         dp[i] %= divisor
         
+    # 2자리 문자를 제외한 앞의 i-2 문자를 표현하는 방법
+    if 10 <= double_digit <= 26:
+        dp[i] += dp[i - 2]
+        dp[i] %= divisor
+
 print(dp[-1])
