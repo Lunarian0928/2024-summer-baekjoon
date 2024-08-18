26bf1f3385ac2a3a81f23d62c5e57ef2d7214f0d
Lunarian0928 <thanks092834@gmail.com>
2024-08-18 16:48:47 +0900

    2839번: 설탕 배달

--- 2839.py
+++ 2839.py
@@ -1,17 +1,21 @@
+# 배달해야 하는 설탕의 무게(kg)
 n = int(input())
 
-num_of_paperbag = 0
-while (n > 0):
-    if ((n - 5) % 5 == 0) or ((n - 5) % 3 == 0):
-        n -= 5
-        num_of_paperbag += 1
-    elif ((n - 3) % 5 == 0) or ((n - 3) % 3 == 0):
-        n -= 3
-        num_of_paperbag += 1
-    else:
+# 5킬로그램 봉지의 개수
+num_of_paperbag5 = n // 5
+
+while True:
+    # 3킬로그램 봉지의 개수
+    # 5킬로그램 봉지의 개수를 반영하여 저장함
+    num_of_paperbag3 = (n - 5 * num_of_paperbag5) // 3
+    if (num_of_paperbag5 < 0):
+        print(-1)
         break
     
-if (n != 0):
-    print(-1)
-else:
-    print(num_of_paperbag)
+    # 봉지에 담겨있는 설탕 무게와 배달해야 하는 설탕과 같은 경우
+    if (5 * num_of_paperbag5 + 3 * num_of_paperbag3 == n):
+        print(num_of_paperbag5 + num_of_paperbag3)
+        break
+    
+    # 5킬로그램 설탕의 개수를 감소시킴
+    num_of_paperbag5 -= 1
