f8cf320d6900e33090c14fec136ee5c63ea044ce
Lunarian0928 <thanks092834@gmail.com>
2024-08-27 16:11:34 +0900

    1822번: 차집합

---
+++ 1822.py
@@ -0,0 +1,35 @@
+def binary_search(target):
+    start = 0
+    end = n2 - 1
+    
+    while True:
+        if (start > end):
+            return True
+        
+        mid = (start + end) // 2
+        
+        mid_value = set2[mid]
+        if (mid_value == target):
+            return False
+        elif (mid_value < target):
+            start = mid + 1
+        else:
+            end = mid - 1
+
+n1, n2 = map(int, input().split())
+
+set1 = list(map(int, input().split()))
+set1.sort()
+
+set2 = list(map(int, input().split()))
+set2.sort()
+
+diff_set = []
+for i in range(n1):
+    target = set1[i]
+    if (binary_search(target)):
+        diff_set.append(target)
+
+print(len(diff_set))
+for elem in diff_set:
+    print(elem, end = ' ')
