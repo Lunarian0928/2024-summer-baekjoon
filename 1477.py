72c8a13553a738e136eff5f0bd57bf19fb9cb39d
Lunarian0928 <thanks092834@gmail.com>
2024-08-31 10:39:39 +0900

    1477번: 휴게소 세우기

---
+++ 1477.py
@@ -0,0 +1,34 @@
+# n: 현재 휴게소의 개수
+# m: 더 지으려고 하는 휴게소의 개수
+# l: 고속도로의 길이
+n, m, l = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+arr = [0] + arr + [l]
+
+# 지을 수 있는 휴게소의 개수가 몇 개인지 확인
+def check_cnt(mid): 
+    cnt = 0
+    for i in range(len(arr) - 1):
+        gap = arr[i + 1] - arr[i]
+        if (gap > mid):
+            cnt += (gap - 1) // mid
+    return cnt <= m 
+
+start = 1
+end = l
+
+while (start <= end):
+    mid = (start + end) // 2
+    
+    # 휴게소의 개수가 m개보다 작거나 같을 경우
+    if (check_cnt(mid)):
+        # 휴게소의 설치할 수 있는 간격을 줄이는 방향으로 탐색한다.
+        end = mid - 1
+    else:
+        # 휴게소를 설치할 수 있는 간격을 늘리는 방향으로 탐색한다.
+        start = mid + 1
+
+print(mid)
