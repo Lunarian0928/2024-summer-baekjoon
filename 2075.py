e5fae781b092680a3ddfe8c1de950ac2f60580b7
Lunarian0928 <thanks092834@gmail.com>
2024-09-21 19:57:08 +0900

    2075번: N번째 큰 수

---
+++ 2075.py
@@ -0,0 +1,13 @@
+import heapq
+n = int(input())
+
+min_heap = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    for num in row:
+        heapq.heappush(min_heap, num)
+        
+        if (len(min_heap)> n):
+            heapq.heappop(min_heap)
+
+print(min_heap[0]) 
